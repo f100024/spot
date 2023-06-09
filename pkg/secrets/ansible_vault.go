82b389a8f7851aaf622bc7db27066acdfbf5585b
f100024 <f100024@ad1.dev>
2023-06-09 11:25:32 +0300

    fix linter warnings;

--- pkg/secrets/ansible_vault.go
+++ pkg/secrets/ansible_vault.go
@@ -15,19 +15,19 @@ type AnsibleVaultProvider struct {
 }
 
 // NewAnsibleVaultProvider creates a new instance of AnsibleVaultProvider
-func NewAnsibleVaultProvider(vault_path, secret string) (*AnsibleVaultProvider, error) {
-	fi, err := os.Lstat(vault_path)
+func NewAnsibleVaultProvider(vaultPath, secret string) (*AnsibleVaultProvider, error) {
+	fi, err := os.Lstat(vaultPath)
 	if err != nil {
-		return nil, fmt.Errorf("error get fileinfo of: %s", vault_path)
+		return nil, fmt.Errorf("error get fileinfo of: %s", vaultPath)
 	}
 	if !fi.Mode().IsRegular() {
-		return nil, fmt.Errorf("%s is not a regular file", vault_path)
+		return nil, fmt.Errorf("%s is not a regular file", vaultPath)
 	}
 
 	// Decrypt ansible-vault
-	decryptedVault, err := vault.DecryptFile(vault_path, secret)
+	decryptedVault, err := vault.DecryptFile(vaultPath, secret)
 	if err != nil {
-		return nil, fmt.Errorf("error decrypting file: %s", vault_path)
+		return nil, fmt.Errorf("error decrypting file: %s", vaultPath)
 	}
 	log.Printf("[INFO] ansible vault file decrypted")
 
@@ -42,8 +42,8 @@ func NewAnsibleVaultProvider(vault_path, secret string) (*AnsibleVaultProvider, 
 
 // Get decrypted data from ansible-vault file
 func (p *AnsibleVaultProvider) Get(key string) (string, error) {
-	if key_value, ok := p.data[key]; ok {
-		return fmt.Sprintf("%v", key_value), nil
+	if keyValue, ok := p.data[key]; ok {
+		return fmt.Sprintf("%v", keyValue), nil
 	}
 	return "", fmt.Errorf("not found key: %v", key)
 }
--- pkg/secrets/ansible_vault_test.go
+++ pkg/secrets/ansible_vault_test.go
@@ -8,9 +8,9 @@ import (
 )
 
 func TestAnsibleVaultProvider_Get(t *testing.T) {
-	vault_path := "testdata/test_ansible-vault"
-	vault_secret := "password"
-	p, err := NewAnsibleVaultProvider(vault_path, vault_secret)
+	vaultPath := "testdata/test_ansible-vault"
+	vaultSecret := "password"
+	p, err := NewAnsibleVaultProvider(vaultPath, vaultSecret)
 	require.NoError(t, err, "failed to create AnsibleVaultProvider")
 
 	t.Run("secret found", func(t *testing.T) {
@@ -26,30 +26,30 @@ func TestAnsibleVaultProvider_Get(t *testing.T) {
 }
 
 func TestAnsibleVaultProvider_Create(t *testing.T) {
-	vault_path := "testdata/test_ansible-vault"
-	vault_path_invalid_yaml := "testdata/test_ansible-vault-invalid-yaml"
-	wrong_vault_file_path := "testdata/wrong-test_ansible-vault"
-	vault_file_path_is_not_regular_file := "testdata/"
-	vault_secret := "password"
-	wrong_vault_secret := "password0"
+	vaultPath := "testdata/test_ansible-vault"
+	vaultPathInvalidYaml := "testdata/test_ansible-vault-invalid-yaml"
+	wrongVaultFilePath := "testdata/wrong-test_ansible-vault"
+	vaultFilePathIsNotRegularFile := "testdata/"
+	vaultSecret := "password"
+	wrongVaultSecret := "password0"
 
 	t.Run("ansible vault not found", func(t *testing.T) {
-		_, err := NewAnsibleVaultProvider(wrong_vault_file_path, vault_secret)
+		_, err := NewAnsibleVaultProvider(wrongVaultFilePath, vaultSecret)
 		require.EqualError(t, err, "error get fileinfo of: testdata/wrong-test_ansible-vault")
 	})
 
 	t.Run("ansible vault is not a file", func(t *testing.T) {
-		_, err := NewAnsibleVaultProvider(vault_file_path_is_not_regular_file, vault_secret)
+		_, err := NewAnsibleVaultProvider(vaultFilePathIsNotRegularFile, vaultSecret)
 		require.EqualError(t, err, "testdata/ is not a regular file")
 	})
 
 	t.Run("ansible vault wrong password", func(t *testing.T) {
-		_, err := NewAnsibleVaultProvider(vault_path, wrong_vault_secret)
+		_, err := NewAnsibleVaultProvider(vaultPath, wrongVaultSecret)
 		require.EqualError(t, err, "error decrypting file: testdata/test_ansible-vault")
 	})
 
 	t.Run("ansible vault error unmarshaling yaml", func(t *testing.T) {
-		_, err := NewAnsibleVaultProvider(vault_path_invalid_yaml, vault_secret)
+		_, err := NewAnsibleVaultProvider(vaultPathInvalidYaml, vaultSecret)
 		require.EqualError(t, err, "error during unmarshaling yaml file")
 	})
 
